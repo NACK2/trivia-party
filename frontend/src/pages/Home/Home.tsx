1b1f46c9b730d4876ae89f8fb8ca1d7e883f1c9b
Nicholas Kang <nicholaskang5@gmail.com>
2026-01-10 11:17:45 -0800

    testing supabase calls

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,8 +1,41 @@
+import { useEffect } from "react";
 import { ActionIcon, Button, Flex, Title, TextInput } from "@mantine/core";
 import { IconChevronRight } from "@tabler/icons-react";
 import styles from "./Home.module.css";
+import supabase from "../../utils/supabase";
 
 function Home() {
+  useEffect(() => {
+    async function fetchData() {
+      const { data, error } = await supabase.from("questions").select("*");
+      console.log("fetch data:", data, "fetch error:", error);
+    }
+
+    async function insertData() {
+      const { data, error } = await supabase
+        .from("questions")
+        .insert([
+          {
+            question: "Who is all time points leader in the NBA?",
+            correct_answer: "LeBron James",
+            categories: ["sports"],
+            difficulty: "easy",
+          },
+        ])
+        .select();
+
+      console.log("insert data:", data, "insert error:", error);
+    }
+
+    async function getCurrUser() {
+      const currUser = await supabase.auth.getUser();
+      console.log(currUser);
+    }
+
+    // fetchData();
+    // insertData();
+    // getCurrUser()
+  }, []);
   return (
     <Flex
       h="100vh"
